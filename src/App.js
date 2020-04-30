6d2fef52b47ee1832d2dde025249c5e1f6470da8
MakerJoseph <1joelmaker@gmail.com>
2020-05-01 02:39:34 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Tweet from './Tweet/Tweet';
+
+function App() {
+  return(
+    <div className="App">
+      <h1>Hello, this is a react app</h1>
+      <Tweet />
+      <Tweet />
+      <Tweet />
+      <Tweet />
+    </div>
+  )
+}
+
+export default App;
