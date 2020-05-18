2ceb36672880460cc46b23ecced55b6128398038
MakerJoseph <1joelmaker@gmail.com>
2020-05-19 00:53:03 +0300

    user icon size

--- src/Tweet/Tweet.js
+++ src/Tweet/Tweet.js
@@ -5,6 +5,7 @@ import React from 'react';
 function Tweet(props){
     return (
       <div className="tweet">
+        <i className="fa fa-user fa-3x"></i>
         <h3>{props.name}</h3>
         <p>{props.message}</p>
         <h3>
